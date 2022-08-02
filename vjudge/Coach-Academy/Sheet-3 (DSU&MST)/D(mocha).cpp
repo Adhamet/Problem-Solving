2915b77f5e31a4ebf091bd8a90cef5f053afa403
Adham Essam <adhamessamet@proton.me>
2022-08-02 23:08:53 +0200

    DSU & MST

--- vjudge/Coach-Academy/Sheet-3 (DSU&MST)/D(mocha).cpp
+++ vjudge/Coach-Academy/Sheet-3 (DSU&MST)/D(mocha).cpp
@@ -7,5 +7,67 @@ using namespace std;
 #define S second
 #define intPair pair<int,int>
 
-const int N = 1e5+10, oo = 1e9;
-int n,m;
+const int N = 1e3+10, oo = 1e9;
+int n;
+
+int findParent(int u, ll par[])
+{
+    if (par[u] == u)
+        return u;
+    return par[u] = findParent(par[u], par);
+}
+void connect(int u, int v, ll par[])
+{
+    u = findParent(u, par);
+    v = findParent(v, par);
+    if ( u == v )
+        return;
+    par[u] = v;
+}
+bool isConn(int u, int v, ll par[])
+{
+    return (findParent(u, par) == findParent(v, par));
+}
+
+int main()
+{
+    fff;
+    cin >> n;
+    int mMocha, mDiana;
+    cin >> mMocha >> mDiana;
+    ll parMocha[n], parDiana[n];
+    for(int i = 1; i <= n; i++)
+    {   
+        parMocha[i] = i;
+        parDiana[i] = i;
+    }
+    for(int i = 1; i <= mMocha; i++)
+    {
+        int u,v;
+        cin >> u >> v;
+        connect(u,v,parMocha);
+    }
+    for(int i = 1; i <= mDiana; i++)
+    {
+        int u,v;
+        cin >> u >> v;
+        connect(u,v,parDiana);
+    }
+
+    if (n == mMocha+1 || n == mDiana+1)
+        return cout << 0 << endl, 0;
+
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; i++)
+        {
+            if ( n == mMocha+1 || n == mDiana+1 )
+                break;
+            else
+                if( i!=j && !isConn(i,j,parMocha) && !isConn(i,j,parDiana))
+                {
+                    connect(i,j,parMocha);
+                    connect(i,j,parDiana);
+                    cout << i << " " << j << endl;
+                }
+        }
+}
