094dd391cae4a9ede2f362dd381e551e6de9e15c
Adham Essam <adhamessamet@proton.me>
2022-08-30 21:31:15 +0200

    Solutions

--- vjudge/Coach-Academy/Sheet-4 (Dijkstra)/M(wisdom).cpp
+++ vjudge/Coach-Academy/Sheet-4 (Dijkstra)/M(wisdom).cpp
@@ -1,3 +1,8 @@
+//`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'//
+//~~~~~~~~UNSOLVED~~~~~~~~~~~~~~~~'//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,7 +10,7 @@ using namespace std;
 #define fff ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define F first
 #define S second
-#define intPair vector<int,int>
+#define intPair pair<int,int>
 const ll N = 1e5+10, oo = 1e18+7;
 int n,m,k;
 ll dist[N];  
@@ -13,14 +18,14 @@ vector<pair<int, pair<ll,ll>>> adj[N];
 
 void Dijkstra(ll wisdom)
 {
-    for(int i = 0; i < n; i++)
-        dist[i] = 1e9;
+    for(int i = 1; i <= n; i++)
+        dist[i] = oo;
     dist[1] = 0;
-    priority_queue< pair<ll, int> > pq;
+    priority_queue< pair<ll, int>, vector<pair<ll,int>>, greater<>> pq;
     pq.push({0,1});
 
     while(!pq.empty()) {
-        ll currNode = pq.top().S, cost = -pq.top().F;
+        ll currNode = pq.top().S, cost = pq.top().F;
         pq.pop();
 
         if(dist[currNode] < cost) continue;
@@ -33,7 +38,7 @@ void Dijkstra(ll wisdom)
             if(wis <= wisdom && dist[next] > dist[currNode] + c)
             {
                 dist[next] = dist[currNode] + c;
-                pq.push({-dist[next], next});
+                pq.push({dist[next], next});
             } 
         } 
     }
@@ -42,29 +47,26 @@ void Dijkstra(ll wisdom)
 
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
+    fff
+    int T;
+    cin >> T;
+    while(T--)
     {
         cin >> n >> m >> k;
-        for(int i=0; i < n; i++)
+        for(int i = 0; i <= n; i++)
             adj[i].clear();
-
-        for(int i=0; i < m; i++)
+        for(int i = 0; i < m; i++)
         {
             int u,v,c,w;
             cin >> u >> v >> c >> w;
-            u--,v--;
-            adj[u].push_back( {v,{c,w}} );
-            adj[v].push_back( {u,{c,w}} );
+            adj[u].push_back({v, {c, w}});
+            adj[v].push_back({u, {c, w}});
         }
-        cout << "TEST NOMBER TWO";
-        int start = 0, end = 1e9, mid , answer = -1;
+        int start = 0, end = 1e9, mid, answer = -1;
         while(start <= n)
         {
             mid = (start+end)>>1;
             Dijkstra(mid);
-            "TEST NOMBER SREE";
             if(dist[n] < k)
             {
                 answer = mid;
