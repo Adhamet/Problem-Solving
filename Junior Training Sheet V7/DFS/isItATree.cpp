0aa9e7c8f089f802d0459d97c60ecbdf66727131
Adham Essam <adhamessamet@proton.me>
2023-01-07 13:36:44 +0200

    dfs problems

--- Junior Training Sheet V7/DFS/isItATree.cpp
+++ Junior Training Sheet V7/DFS/isItATree.cpp
@@ -1,28 +1,35 @@
+// SPOJ
 #include <bits/stdc++.h>
+ 
 using namespace std;
-
+#define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define ll long long
-#define fff ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
-const int N = 1e4+10;
-// Adjancency list:
+#define lld long long double
+#define intPair pair<int,int>
+#define llPair pair<ll,ll>
+#define ldPair pair<lld,lld>
+#define F first
+#define S second
+#define el '\n'
+const int N = 1e4+10, oo = 1e9+2; 
 vector<int> adj[N];
 bool vis[N];
+
 void dfs(int node)
 {
-    vis[node] = true;
-    for(auto child : adj[node])
+    vis[node] = 1;
+    for(auto child: adj[node])
         if(!vis[child])
             dfs(child);
 }
 
-// Checking cyclicity;
+// Check connectivity
 bool vis2[N];
 bool notCyclic = false;
 void dfs2(int node, int parent)
 {
-    vis2[node] = true;
-    for(auto child : adj[node])
+    vis2[node] = 1;
+    for(auto child: adj[node])
     {
         if(!vis2[child])
             dfs2(child, node);
@@ -30,14 +37,13 @@ void dfs2(int node, int parent)
             notCyclic = true;
     }
 }
-// --------------------
-
+ 
 int main()
 {
-    fff;
+    adhamet;
+ 
     int n,m;
     cin >> n >> m;
-    
     for(int i = 0; i < m; i++)
     {
         int u,v;
@@ -46,13 +52,11 @@ int main()
         adj[v].push_back(u);
     }
     dfs(1);
-    // First you check if it's connected:
-    for(int i=1; i <= n; i++)
+    for(int i = 1; i <= n; i++)
         if(!vis[i])
-            return cout << "NO\n", 0;
-    // Then check if Acyclic or not:
-    dfs2(1, 0);
-    if (!notCyclic)
+            return cout << "NO\n",0;
+    dfs2(1,0);
+    if(!notCyclic)
         cout << "YES\n";
     else cout << "NO\n";
 }
