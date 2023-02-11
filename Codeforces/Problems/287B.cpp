9cce465d8343d53df54042013263f44b249231a7
Adham Essam <adhamessamet@proton.me>
2023-02-11 19:38:52 +0200

    Name: Pipelines, BSFrstFind, Speed: 15ms, Memory: 0KB

--- Codeforces/Problems/287B.cpp
+++ Codeforces/Problems/287B.cpp
@@ -11,28 +11,61 @@ using namespace std;
 #define S second
 #define el '\n'
 
-void solve()
+// sum: 1,2,3...n..
+ll sum(ll n)
 {
-    int n,k,i=0,result=0;
-    cin >> n >> k;
+    return (n * (n+1)) / 2;
+}
 
-    if( n == k )
-        cout << 1;
-    else
-        cout << -1;
+// sum: s,s+1,s+2...e..
+ll sum(ll s, ll e)
+{
+    if (s<=1)
+        return sum(e);
+    return sum(e) - sum(s-1);
+}
 
-    while(k < n && result < n) {
-        i++;
-        result = k;
-        result*=i;
+ll minSplitters(ll k, ll n)
+{
+    ll st = 1, en = k;
+    
+    while(st < en)
+    {
+        ll md = (st + en)/2;
+        ll s = sum(md, k);
+
+        if(s == n)
+            return k - md + 1;
+        
+        if(s > n)
+            st = md+1;
+        else
+            en = md;
     }
-    cout << i;
+    
+    return k - st + 2;
 }
 
 int main()
 {
     adhamet
     
-    solve();
+    ll n,k;
+    cin >> n >> k;
+
+    if (n == 1)
+        cout << 0 << el;
+    else if(n <= k)
+        cout << 1 << el;
+    else
+    {
+        --k;
+        --n;
+
+        if(sum(k) < n)
+            cout << -1 << el; 
+        else
+            cout << minSplitters(k,n) << el;
+    }
     return 0;
-}
+}   
