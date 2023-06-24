9fe550e443e4dfe5adb69ed1267d6078b9af6459
Adham Essam <adhamessamet@proton.me>
2023-06-24 19:10:07 +0300

    Added Uber-Driver problem

---
+++ Codeforces/Polygon/Uber Driver/Solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+int main()
+{
+    adhamet;
+
+    unsigned long long n;
+    cin >> n;
+    cout << n*(n+1)/2;
+
+    return 0;
+}
---
+++ Codeforces/Problems/711A.cpp
@@ -0,0 +1,27 @@
+    // Lecture sleep
+    #include <bits/stdc++.h>
+    #include <algorithm>
+    using namespace std;
+    #define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #define ll long long
+    #define lld long long double
+    #define intPair pair<int,int>
+    #define llPair pair<ll,ll>
+    #define ldPair pair<lld,lld>
+    #define F first
+    #define S second
+    #define el '\n'
+
+    void solve()
+    {
+
+    }
+
+    int main()
+    {
+        adhamet;
+
+        solve();
+
+        return 0;
+    }
