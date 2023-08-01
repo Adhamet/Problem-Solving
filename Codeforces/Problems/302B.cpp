db5c92e2adce6271423370ed2535ffa10cee949f
Adham Essam <adhamessamet@proton.me>
2023-08-01 19:07:52 +0300

    Sum Is _ Or Eq, speed:200ms

--- Codeforces/Problems/302B.cpp
+++ Codeforces/Problems/302B.cpp
@@ -1,24 +1,47 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-int main()
+using namespace std;
+#define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+#define lld long long double
+#define ull unsigned long long
+#define intPair pair<int,int>
+#define llPair pair<ll,ll>
+#define ldPair pair<lld,lld>
+#define el '\n'
+
+int main() 
 {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n,m;
+/* #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+#endif */
+    adhamet
+    
+    int n,m,c,t;
     cin >> n >> m;
-    
-    int a[n], b[n];
-    b[0]=0;
+
+    int arr[n+2];
+    arr[0] = 1;
     for(int i = 1; i <= n; i++) {
-        int x,y;
-        cin >> x >> y;
-        a[i] = b[i-1] + 1;
-        b[i] = b[i-1] + x * y;
+        cin >> c >> t;
+        arr[i] = arr[i-1] + c * t;
     }
-    
-    int val;
-    while(m--) {
-        cin >> val;
-        cout << lower_bound(b + 1, b + 1 + n, val) - b << endl;
+
+    int min;
+    int l, r, mid, ans;
+    while (m--)
+    {
+        l = 1, r = n;
+        cin >> min;
+        
+        while (l != r) {
+            mid = l + (r - l)/2;
+
+            if ( arr[mid] < min ) l = mid + 1;
+            else if ( arr[mid] > min ) r = mid;
+            else l = mid + 1;
+        }
+        cout << l << el;
     }
+	return 0;
 }
