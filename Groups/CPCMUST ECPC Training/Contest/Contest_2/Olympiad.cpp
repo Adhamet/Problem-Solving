f6c6be89512035884c7c16d01dcd26c3651dceb9
Adham Essam <adhamessamet@proton.me>
2023-08-08 15:08:19 +0300

    Olympiad, speed: 126ms

--- Groups/CPCMUST ECPC Training/Contest/Contest_2/Olympiad.cpp
+++ Groups/CPCMUST ECPC Training/Contest/Contest_2/Olympiad.cpp
@@ -18,7 +18,33 @@ int main()
     
     adhamet;
     
-    
+        int n,l,r,x,cnt=0;
+    cin>>n>>l>>r>>x;
+    int arr[n];
+
+    for(int i=0; i<n; i++)cin>>arr[i];
+ 
+    vector<vector<int>>ans;
+    for(int msk=0; msk<(1<<n); msk++){
+        int mn=INT_MAX,mx=-INT_MAX;
+        vector<int>temp;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            if((msk>>i)&1){
+                if(arr[i]<mn) mn=arr[i];
+                if(arr[i]>mx) mx=arr[i];
+                temp.push_back(arr[i]);
+                sum += arr[i];
+            }
+        }
+        if((sum>=l&&sum<=r)&&(abs(mn-mx)>=x)&&temp.size()>=2)
+        {
+            ans.push_back(temp);
+            cnt++;
+        }
+        //ans.insert(temp);
+    }
+    cout<<cnt;
  
 	return 0;
 }
