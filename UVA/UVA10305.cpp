54b1ed2664c81b5e88ce2a49dd316db9f48a881f
Adham Essam <adhamessamet@proton.me>
2023-09-07 12:02:31 +0300

    accepted

--- UVA/UVA10305.cpp
+++ UVA/UVA10305.cpp
@@ -1,60 +1,59 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
 #define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define ll long long
 #define lld long long double
+#define ull unsigned long long
 #define intPair pair<int,int>
 #define llPair pair<ll,ll>
 #define ldPair pair<lld,lld>
-#define F first
-#define S second
 #define el '\n'
-const int N = 1e4+10, oo = 1e9+2; 
-vector<int> adj[N];
-vector<int> result;
-bool vis[N];
 
-void dfs(int node)
-{
-    vis[node] = 1;
-    for(auto child: adj[node])
-        if(!vis[child])
-            dfs(child);
-    result.push_back(node);
+const int N = 100+5;
+vector< vector<int> > adjL(N);
+bool vis[N];
+vector<int> topological_sort;
+
+void dfs(int node) {
+    vis[node]=1;
+    for(auto child: adjL[node]) if (!vis[child])
+        dfs(child);
+    topological_sort.push_back(node);
 }
- 
-int main()
-{
-    adhamet;
+
+int main() 
+{/* #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+#endif */
+    adhamet
     
-    while(true)
+    int n,m;
+    while(cin >> n >> m)
     {
-        result.clear();
+        topological_sort.clear();
         for(int i = 0; i < N; i++)
         {
-            adj[i].clear();
+            adjL[i].clear();
             vis[i]=0;
         }
 
-        int n,m,u,v;
-        cin >> n >> m;
-        if(n+m==0) break;
+        if ( n + m == 0 ) break;
 
+        int u,v;
         for(int i = 0; i < m; i++)
         {
             cin >> u >> v;
-            adj[v].push_back(u);
+            adjL[v].push_back(u);
         }
-        dfs(1);
-        for(int i = 2; i <= n; i++)
-            if(!vis[i])
-                dfs(i);
+
+        for(int i = 1; i <= n; i++) if (!vis[i])
+            dfs(i);
         
-        for(auto item : result)
+        for(auto item: topological_sort)
             cout << item << " ";
         cout << el;
     }
 
-    return 0;
+	return 0;
 }
