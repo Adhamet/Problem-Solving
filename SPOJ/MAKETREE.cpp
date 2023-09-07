9f2a99f809a38bafb90f6f52608ce7e6e0907739
Adham Essam <adhamessamet@proton.me>
2023-09-07 11:36:41 +0300

    speed:170ms

--- SPOJ/MAKETREE.cpp
+++ SPOJ/MAKETREE.cpp
@@ -1,65 +1,58 @@
-//SPOJ
 #include <bits/stdc++.h>
 
 using namespace std;
 #define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define ll long long
 #define lld long long double
+#define ull unsigned long long
 #define intPair pair<int,int>
 #define llPair pair<ll,ll>
 #define ldPair pair<lld,lld>
- 
-int n, s, wishes, pos, a;
-vector<vector<int> > adj;
-bool vis[100074];
-vector<int> result;
-int parent[100074];
- 
-void dfs(int student) {
-	vis[student] = true;
-	for (int i = 0; i < (int) adj[student].size(); i++) {
-		int child = adj[student][i];
-		if (!vis[child])
-			dfs(child);
-	}
-	result.push_back(student);
+#define el '\n'
+
+const int N = 1e5+5;
+vector< vector<int> > adjL(N);
+bool vis[N];
+int parent[N];
+vector<int> topological_sort;
+
+void dfs(int node) {
+    vis[node]=1;
+    for(auto child: adjL[node]) if (!vis[child])
+        dfs(child);
+    topological_sort.push_back(node);
 }
- 
-int main() {
- 
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-#endif
+
+int main() 
+{/* #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+#endif */
+    adhamet
     
-    adhamet;
+    int n,k,w,x;
+    cin >> n >> k;
+
+    for(int i = 1; i <= k; i++) 
+    {
+        cin >> w;
+        while(w--) {
+            cin >> x;
+            adjL[i].push_back(x);
+        }
+    }
+
+    for(int i = 1; i <= n; i++) if (!vis[i])
+        dfs(i);
     
-	scanf("%d%d", &n, &s);
- 
-	adj.resize(n + 1);
- 
-	for (int i = 1; i <= s; i++) {
-		scanf("%d", &wishes);
-		for (int j = 0; j < wishes; j++) {
-			scanf("%d", &a);
-			adj[i].push_back(a);
-		}
-	}
- 
-	for (int i = 1; i <= n; i++) {
-		if (!vis[i])
-			dfs(i);
-	}
- 
-	pos = 0;
-	for (int i = n - 1; i >= 0; i--) {
-		int Student = result[i];
-		parent[Student] = pos;
-		pos = Student;
-	}
- 
-	for (int i = 1; i <= n; i++) {
-		printf("%d\n", parent[i]);
-	}
- 
+    int pos=0, student;
+    for(int i = n - 1; i >= 0; i--) {
+        student = topological_sort[i];
+        parent[student] = pos;
+        pos = student;
+    }
+
+    for(int i = 1; i <= n; i++)
+        cout << parent[i] << el;
+
 	return 0;
 }
