135041be0b6facd1967784901156dd9311b86d19
Adham Essam <adhamessamet@proton.me>
2023-09-18 18:00:38 +0300

    upsolved acc

--- Contests/atcoder/BeginnerContest320/B_Longest_Palindrome.cpp
+++ Contests/atcoder/BeginnerContest320/B_Longest_Palindrome.cpp
@@ -1,12 +1,6 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+#include <string>
 using namespace std;
-using namespace __gnu_pbds;
-template<class T, class U = null_type, class chash = hash<T>> using hset = 
-gp_hash_table<T,U,chash>;
-template<class T, class U = null_type, class cmp = less<T>> using oset = 
-tree<T,U,cmp,rb_tree_tag,tree_order_statistics_node_update>;
 
 #define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using ll = long long;
@@ -15,11 +9,6 @@ using ld = long double;
 using pi = pair<int,int>;
 using pll = pair<ll,ll>;
 using pd = pair<ld,ld>;
-using vi = vector<int>;
-using vb = vector<bool>;
-using vll = vector<ll>;
-using vpi = vector<pi>;
-using vpll = vector<pll>;
 #define el '\n'
 #define dbg(v)                                                                 \
 	cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
