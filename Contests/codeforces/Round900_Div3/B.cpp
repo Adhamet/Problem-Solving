638390a5132f58038e59b1dbc8ad9c19b2893520
Adham Essam <adhamessamet@proton.me>
2024-08-29 11:46:52 +0300

    acc

---
+++ Contests/codeforces/Round900_Div3/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#define F first
+#define S second
+#define el '\n'
+
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		for (int i = 1; i <= n; i++) cout << 3 * i + 1 << " ";
+		cout << el;
+	}
+	
+	return 0;
+}
