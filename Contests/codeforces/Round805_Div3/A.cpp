e76baf9a7e55d28263ebce013711a760785e2757
Adham Essam <adhamessamet@proton.me>
2024-09-23 11:31:48 +0300

    acc

---
+++ Contests/codeforces/Round805_Div3/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#define F first
+#define S second
+#define el '\n'
+
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+/*const int N = 20 + 5;*/
+/*const int oo = 1e9 + 20;*/
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	
+	int t;
+	cin >> t;
+	
+	while (t--) { 
+		int m;
+		cin >> m;
+
+		int k = 1;
+		while (k <= m) k *= 10;
+		k /= 10;
+
+		cout << m - k << el;
+	}
+	
+	return 0;
+}
