b57dfe7e69ed37688dddfff8bb7ea5fd38a656d1
Adham Essam <adhamessamet@proton.me>
2024-09-23 11:34:31 +0300

    acc

---
+++ Contests/codeforces/Round811_Div3/C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#define F first
+#define S second
+#define el '\n'
+
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+/*const int N = 20 + 5;*/
+/*const int oo = 1e9 + 20;*/
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		string s = "";
+		for (int i = 9; i >= 1; i--) {
+			if (n >= i) s += ('0' + i), n -= i;
+		}
+
+		reverse(s.begin(), s.end());
+		cout << s << el;
+	}
+
+	return 0;
+}
