7ad3a5c09243454d4076efb656418f66c2beab8b
Adham Essam <adhamessamet@proton.me>
2024-11-02 23:55:11 +0200

    acc

--- uva/11231 Black And White Painting/11231.cpp
+++ uva/11231 Black And White Painting/11231.cpp
@@ -1,28 +1,20 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-#define adhamet ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-#define ll long long
-#define lld long long double
-#define intPair pair<int,int>
-#define llPair pair<ll,ll>
-#define ldPair pair<lld,lld>
-#define F first
-#define S second
+
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
 #define el '\n'
+#define MOD 1000000007
+using i64 = long long;
+using pi = pair<int, int>;
 
-void solve(int n,int m, int c)
-{
-    if ( c == 0 ) { cout << (n-7)*(m-7)/2 << el; }
-    else { cout << ( (n-7)*(m-7)+1 )/2 << el; }
-}
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0);
 
-int main()
-{
-    adhamet
+	i64 x, y, z;
+	while (cin >> x >> y >> z && (x || y || z)) {
+		if (z == 0) cout << (x - 7) * (y - 7) / 2 << el;
+		else cout << ((x - 7) * (y - 7) + 1) / 2 << el;
+	}
 
-    int n,m,c;
-    while(cin >> n >> m >> c && n+m+c != 0)
-        solve(n,m,c);
-    return 0;
+	return 0;
 }
