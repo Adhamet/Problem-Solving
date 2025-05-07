891dac89d1a7ef0976c643eb697049b128d20645
Adham Essam <adhamessamet@proton.me>
2025-05-07 18:15:20 +0300

    acc

--- codeforces/Problems/920F SUM and REPLACE/920F.cpp
+++ codeforces/Problems/920F SUM and REPLACE/920F.cpp
@@ -5,63 +5,70 @@ using namespace std;
 #define int i64
 #define el '\n'
 
-const int MAXN = 1e5+5, MXD = 1e6+5;
+const int MAXN = 3e5 + 5;
+const int MXD = 1e6 + 5;
 vector<int> D(MXD);
-int t[4*MAXN];
+struct Node {
+    int sum = 0, cnt = 0;
+};
 
-void build(vector<int> a, int v, int tl, int tr) {
-	if (tl == tr) t[v] = a[tl];
-	else {
-		int tm = (tl + tr) >> 1;
-		build(a, v*2, tl, tm);
-		build(a, v*2+1, tm+1, tr);
-		t[v] = t[v*2] + t[v*2+1];
-	}
+Node t[4 * MAXN];
+
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v].sum = a[tl];
+        t[v].cnt = (a[tl] > 2);
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, 2*v, tl, tm);
+        build(a, 2*v+1, tm+1, tr);
+        t[v].sum = t[2*v].sum + t[2*v+1].sum;
+        t[v].cnt = t[2*v].cnt + t[2*v+1].cnt;
+    }
 }
 
 void update(int v, int tl, int tr, int l, int r) {
-	if (l > r || tr < l || tl > r) return;
-	if (tl == tr) t[v] = D[t[v]];
-	else {
-		int tm = (tl + tr) >> 1;
-		update(v*2, tl, tm, l, r);
-		update(v*2+1, tm+1, tr, l, r);
-		t[v] = t[v*2] + t[v*2+1];
-	}
+    if (l > r || tr < l || tl > r || t[v].cnt == 0) return;
+    if (tl == tr) {
+        t[v].sum = D[t[v].sum];
+        t[v].cnt = (t[v].sum > 2);
+    } else {
+        int tm = (tl + tr) / 2;
+        update(2*v, tl, tm, l, r);
+        update(2*v+1, tm+1, tr, l, r);
+        t[v].sum = t[2*v].sum + t[2*v+1].sum;
+        t[v].cnt = t[2*v].cnt + t[2*v+1].cnt;
+    }
 }
 
 int query(int v, int tl, int tr, int l, int r) {
-	if (l > r || tr < l || tl > r) return 0;
-	if (tl >= l && tr <= r) return t[v];
-	int tm = (tl + tr) >> 1;
-	return query(v*2, tl, tm, l, r) +
-		query(v*2+1, tm+1, tr, l, r);
+    if (l > r || tr < l || tl > r) return 0;
+    if (tl >= l && tr <= r) return t[v].sum;
+    int tm = (tl + tr) / 2;
+    return query(2*v, tl, tm, l, r) + query(2*v+1, tm+1, tr, l, r);
 }
 
 int32_t main() {
-	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-	for (int i = 1; i < MXD; ++i)
-		for (int j = i; j < MXD; j += i)
-			++D[j];
+    for (int i = 1; i < MXD; ++i)
+        for (int j = i; j < MXD; j += i)
+            ++D[j];
 
-	int n, m;
-	cin >> n >> m;
-	vector<int> a(n);
-	for (int i = 0; i < n; ++i) cin >> a[i];
+    int n, m;
+    cin >> n >> m;
+    int a[n];
+    for (int i = 0; i < n; ++i) cin >> a[i];
 
-	build(a, 1, 0, n - 1);
+    build(a, 1, 0, n - 1);
 
-	while (m--) {
-		int type; cin >> type;
-		if (type == 1) {
-			int l, r; cin >> l >> r;
-			update(1, 0, n - 1, l - 1, r - 1);
-		} else {
-			int l, r; cin >> l >> r;
-			cout << query(1, 0, n - 1, l - 1, r - 1) << el;
-		}
-	}
+    while (m--) {
+        int type, l, r;
+        cin >> type >> l >> r;
+        if (type == 1) update(1, 0, n - 1, l - 1, r - 1);
+        else cout << query(1, 0, n - 1, l - 1, r - 1) << el;
+    }
 
-	return 0;
+    return 0;
 }
+
