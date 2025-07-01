c8950fbeef8d3d81c906fbc70e93f95b1b080f7c
Adhamet <adhamessammetwally@gmail.com>
2025-07-01 20:16:35 +0300

    acc

---
+++ Contests/codeforces/Round1034_Div3/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define i64 int64_t
+#define el '\n'
+
+int32_t main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	int t;	cin >> t;
+	while (t--) {
+		int n;	cin >> n;
+		cout << (n % 4 == 0? "Bob": "Alice") << el;
+	}
+
+	return 0;
+}
