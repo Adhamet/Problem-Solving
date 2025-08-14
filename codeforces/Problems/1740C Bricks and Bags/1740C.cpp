517f6301bb97c762168175c879a9ff2e9c1c291e
Adhamet <adhamessammetwally@gmail.com>
2025-08-14 11:44:50 +0300

    acc

--- codeforces/Problems/1740C Bricks and Bags/1740C.cpp
+++ codeforces/Problems/1740C Bricks and Bags/1740C.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define i64 int64_t
 #define el '\n'
 #define debug(...)
-/*#define debug(...) cerr << __VA_ARGS__ << el;*/
+/*#define debug(...) cerr << __VA_ARGS__ << ' ';*/
 
 int32_t main() {
 	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -11,13 +11,28 @@ int32_t main() {
 	int t;	cin >> t;
 	while (t--) {
 		int n;	cin >> n;
-		vector<i64> a(n);
-		for (int i = 0; i < n; ++i) cin >> a[i];
-		sort(a.begin(), a.end());
+		unordered_set<i64> in;
+		vector<i64> a;
+		for (int i = 0; i < n; ++i) {
+			i64 x;	cin >> x;
+			if (!in.count(x)) a.push_back(x), in.insert(x);
+		}
 
-		i64 cand1 = (a[n - 1] - a[0]) + (a[1] - a[0]);
-		i64 cand2 = (a[n - 1] - a[0]) + (a[n - 1] - a[n - 2]);
-		cout << max(cand1, cand2) << el;
+		int m = (int)a.size();
+		sort(a.begin(), a.end());
+		/*for (auto &x: a) debug(x); cout << el;*/
+		
+		if (m < 2) {
+			cout << 0 << el;
+			continue;
+		}
+
+		i64 ans = (a[m-1] - a[0]) + max(a[1] - a[0], a[m-1] - a[m-2]);
+		for (int j = 2; j < m; ++j)
+			ans = max(ans, (a[j] - a[j - 1]) + (a[j] - a[0]));
+		for (int j = 0; j <= m - 3; ++j)
+			ans = max(ans, (a[j + 1] - a[j]) + (a[m - 1] - a[j]));
+		cout << ans << el;
 	}
 
 	return 0;
