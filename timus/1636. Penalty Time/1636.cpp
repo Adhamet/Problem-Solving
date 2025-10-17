68d054c7a3383016b725dd2c809e2784df407bd2
Adham Essam <adhamessamet@proton.me>
2025-10-17 15:08:41 +0300

    acc

--- timus/1636. Penalty Time/1636.cpp
+++ timus/1636. Penalty Time/1636.cpp
@@ -6,8 +6,14 @@ int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	
-	int n;	std::cin >> n;
-	std::cout << "HELLO \n \n n";
-	
+	i64 t1, t2;		std::cin >> t1 >> t2;
+	i64 cnt = 0;
+	for (int i = 0; i < 10; ++i) {
+		i64 x;	std::cin >> x;
+		cnt += x;
+	}
+	if (t1 + (cnt * 20) <= t2) std::cout << "No chance.";
+	else std::cout << "Dirty debug :(";
+
 	return 0;
 }
